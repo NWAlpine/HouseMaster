d5744ab11ceb24648ae1a538f70cdf88913ef52d
Eric <Eric@ERICPC>
2017-01-20 20:40:20 -0800

    Add project files.

---
+++ HouseMaster/HomeMasterClient.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Networking;
+using Windows.Networking.Sockets;
+using Windows.Storage.Streams;
+
+namespace HouseMaster
+{
+    class HomeMasterClient
+    {
+        private readonly string _ip;
+        private readonly int _port;
+        private StreamSocket _socket;
+        private DataWriter _writer;
+        private DataReader _reader;
+
+        public delegate void Error(string message);
+        public event Error OnError;
+
+        public delegate void DataReceived(string message);
+        public event DataReceived OnDataReceived;
+
+        public string Ip
+        {
+            get { return _ip; }
+        }
+        public int Port
+        {
+            get { return _port; }
+        }
+
+        // ctor
+        public HomeMasterClient(string ip, int port)
+        {
+            _ip = ip;
+            _port = port;
+        }
+
+        /// <summary>
+        /// Connect to endpoint
+        /// </summary>
+        public async void Connect()
+        {
+            try
+            {
+                var hostName = new HostName(Ip);
+                _socket = new StreamSocket();
+                await _socket.ConnectAsync(hostName, Port.ToString());
+                _writer = new DataWriter(_socket.OutputStream);
+                Read();
+
+            }
+            catch (Exception e)
+            {
+                if (OnError != null)
+                    OnError(e.Message);
+            }
+        }
+
+        /// <summary>
+        /// Send Data
+        /// </summary>
+        /// <param name="message">Message to send</param>
+        public async void Send(string message)
+        {
+            // sends the size of the string
+            _writer.WriteUInt32(_writer.MeasureString(message));
+            // sends the string (message)
+            _writer.WriteString(message);
+
+            try
+            {
+                // performs post
+                await _writer.StoreAsync();
+
+                // clean up for next message
+                await _writer.FlushAsync();
+
+            }
+            catch (Exception e)
+            {
+                if (OnError != null)
+                    OnError(e.Message);
+            }
+        }
+
+        /// <summary>
+        /// Read Data
+        /// </summary>
+        private async void Read()
+        {
+            _reader = new DataReader(_socket.InputStream);
+            try
+            {
+                while(true)
+                {
+                    uint sizeFieldCount = await _reader.LoadAsync(sizeof(uint));
+                    // if disconnected
+                    if (sizeFieldCount != sizeof(uint))
+                        return;
+
+                    uint stringLength = _reader.ReadUInt32();
+                    uint actualStringLength = await _reader.LoadAsync(stringLength);
+                    // if disconnected
+                    if (stringLength != actualStringLength)
+                        return;
+
+                    if (OnDataReceived != null)
+                    {
+                        OnDataReceived(_reader.ReadString(actualStringLength));
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                if (OnError != null)
+                    OnError(e.Message);
+            }
+        }
+
+        /// <summary>
+        /// Close and cleanup
+        /// </summary>
+        public void Close()
+        {
+            _writer.DetachStream();
+            _writer.Dispose();
+
+            _reader.DetachStream();
+            _reader.Dispose();
+
+            _socket.Dispose();
+        }
+    }
+}
---
+++ HouseMaster/MainPage.xaml.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace HouseMaster
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        HomeMasterClient _socket;
+
+        // degree symbol = alt+0176 
+        const string celciusSymbol = "°C";
+        const string fahrenheitSymbol = "°F";
+        bool isCelcius = true;  //default
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            txtPort.Text = "9000";
+            txtIp.Text = "192.168.0.24";
+        }
+
+        private void btnConnect_Click(object o, RoutedEventArgs e)
+        {
+            if (_socket != null)
+            {
+                _socket.Close();
+                _socket.OnDataReceived -= Socket_OnDataRecived;
+                _socket = null;
+            }
+
+            _socket = new HomeMasterClient(txtIp.Text, Convert.ToInt32(txtPort.Text));
+            _socket.Connect();
+            _socket.OnDataReceived += Socket_OnDataRecived;
+
+            txbStatus.Text = "Running...";
+
+            //TODO: NYI disconnect
+            //btnConnect.Content = "Disconnect";
+        }
+
+        private void Socket_OnDataRecived(string data)
+        {
+            // parse out new data and display
+            if (!string.IsNullOrEmpty(data))
+            {
+                DisplayData(data);
+            }
+        }
+
+        private void btnSend_Click(object sender, RoutedEventArgs e)
+        {
+            _socket.Send(txtMessage.Text);
+        }
+
+        private void DisplayData(string data)
+        {
+            string sensorId = data.Substring(0, 1);
+            string reading = data.Substring(1);
+            int value;
+            Int32.TryParse(reading, out value);
+
+            switch (sensorId)
+            {
+                case "g":
+                    // garage door
+                    txbGarDoorValue.Text = DoorStateString(value);
+                    break;
+
+                case "k":
+                    // kitchen door
+                    txbKitDoorValue.Text = DoorStateString(value);
+                    break;
+
+                case "l":
+                    // garage light
+                    txbGarLightsValue.Text = LightStateString(value);
+                    break;
+
+                case "a":
+                    // bay A
+                    txbGarBayAValue.Text = BayStateString(value);
+                    break;
+
+                case "i":
+                    txbGarTempValue.Text = string.Format("{0}{1}", value.ToString(), isCelcius ? celciusSymbol : fahrenheitSymbol);
+                    break;
+
+                case "c":
+                    // front temp
+                    txbFrontTempValue.Text = string.Format("{0}{1}", value.ToString(), isCelcius ? celciusSymbol : fahrenheitSymbol);
+                    break;
+
+                case "h":
+                    // front humidity
+                    txbFronthumidValue.Text = string.Format("{0}{1}", value.ToString(), "%");
+                    break;
+                    
+                case "x":
+                    // front HeatIndex
+                    txbFrontHIValue.Text = string.Format("{0}{1}", value.ToString(), isCelcius ? celciusSymbol : fahrenheitSymbol);
+                    break;
+
+                default:
+                    break;
+            }
+
+            #region sample data
+
+            /*
+            GarageDoorState: Closed = 0, Open = 1, Closing = 2, , Opening = 3;
+
+            example data:
+            g0 - Garage Closed
+            g1 - Garage Open
+            g2 - Garage Closing...
+            g3 - Garage Opening...
+
+            k0 - Kitchen Closed
+            K1 - Kitchen Open
+
+            l0 - Light Off
+            l1 - Light On
+
+            a0 - Garage Bay 1 Occupied
+            a1 - Garage Bay 1 Vacant
+
+            b0 - Garage Bay 2 Occupied
+            b1 - Garage Bay 2 Vacant
+            
+
+            tc16.90 - Temp C
+            th59.70 - Humidity
+            ta16.20 - Heat Index C
+            ti16.95 - Internal Temp C (from breakout board)
+
+            gu1 - Garage Upper
+            gl0 - Garage Lower
+            kd1 - Kitchen Door
+            la145 - Light Reading
+            sa117 - Sonar Reading A
+            sb117 - Sonar Reading B
+            tc16.90 - Temp C
+            th59.70 - Humidity
+            ta16.20 - Heat Index C
+            ti16.95 - Internal Temp C (from breakout board)
+
+            */
+
+            #endregion
+        }
+
+        private void AnalyzeTempData(int data)
+        {
+            // logic for F vs C
+
+        }
+
+        private string DoorStateString(int data)
+        {
+            string statusString = string.Empty;
+            switch (data)
+            {
+                case 0:
+                    statusString = "Closed";
+                    break;
+
+                case 1:
+                    statusString = "Open";
+                    break;
+
+                case 2:
+                    statusString = "Closing...";
+                    break;
+
+                case 3:
+                    statusString = "Opening...";
+                    break;
+
+                default:
+                    break;
+            }
+
+            return statusString;
+        }
+
+        private string LightStateString(int data)
+        {
+            string statusString = string.Empty;
+            switch (data)
+            {
+                case 0:
+                    statusString = "Off";
+                    break;
+
+                case 1:
+                    statusString = "On";
+                    break;
+
+                default:
+                    break;
+            }
+
+            return statusString;
+        }
+
+        private string BayStateString(int data)
+        {
+            string statusString = string.Empty;
+            switch (data)
+            {
+                case 0:
+                    statusString = "Occupied";
+                    break;
+
+                case 1:
+                    statusString = "Vacant"; 
+                    break;
+
+                default:
+                    break;
+            }
+
+            return statusString;
+        }
+
+    }
+}
